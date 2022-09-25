0bc646645c1433ebbbe0b0e697d598ce3e55907f
Yutaka <y.tajika529@gmail.com>
2022-09-25 11:02:10 +0900

    fixed: build with xcode

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         .testTarget(
             name: "SemanticVersioningExtendedTests",
             dependencies: ["SemanticVersioningExtended", "Mocks"]),
-        .testTarget(
+        .target(
             name: "Mocks",
             dependencies: ["SemanticVersioning"]),
     ]
